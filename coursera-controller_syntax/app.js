81d18d4bfcc9c28f0e5d461c967a2316369ab01c
cumathka <cumakaya2531@gmail.com>
2024-02-25 09:15:19 +0100

    inheritance ok

--- coursera-controller_syntax/app.js
+++ coursera-controller_syntax/app.js
@@ -3,8 +3,7 @@
     angular.module("ControllerAsApp",[])
     .controller("ParentController1",ParentController1)
     .controller("ChildController1", ChildController1)
-    .controller("ParentController2",ParentController2)
-    .controller("ChildController2", ChildController2);
+
 
     ParentController1.$inject = ["$scope"];
     function ParentController1($scope){
